b3bf5581afbcb6dd3d694ece51643c4afc99ebd5
Xargam <leoxargam@gmail.com>
2018-11-03 19:25:58 -0300

    Agregado TP3 - Faltan clases de archivos y revisación general de clases

---
+++ [LAB II]  TP3.Leonardo.Manassali.2A/Manassali.Leonardo.2A.TP3/Excepciones/ArchivosExeption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Excepciones
+{
+	public class ArchivosException : Exception
+	{
+		#region Constructor
+		public ArchivosException(Exception innerException) : base("Se produjo un error durante la manipulación de archivos.",innerException)
+		{
+		}
+		#endregion
+	}
+}
---
+++ [LAB II]  TP3.Leonardo.Manassali.2A/Manassali.Leonardo.2A.TP3/Excepciones/SinProfesorException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excepciones
+{
+	public class SinProfesorException : Exception
+	{
+		#region Constructor
+		public SinProfesorException() : base("No hay un profesor que dicte la clase.")
+		{
+		}
+		#endregion
+	}
+}
