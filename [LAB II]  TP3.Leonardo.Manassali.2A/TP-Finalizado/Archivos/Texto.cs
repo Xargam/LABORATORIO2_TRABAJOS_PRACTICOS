d29260b0334d5ddad886eaf77aab7c14873acc91
Xargam <leoxargam@gmail.com>
2018-11-03 22:27:46 -0300

    TP3 - Funcionó serializacion

---
+++ [LAB II]  TP3.Leonardo.Manassali.2A/TP-Finalizado/Archivos/Texto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Excepciones;
+
+namespace Archivos
+{
+	public class Texto : IArchivo<string>
+	{
+		#region MetodosIArchivo
+		public bool Guardar(string archivo, string datos)
+		{
+			try
+			{
+				using (StreamWriter textArch = new StreamWriter(archivo))
+				{
+					textArch.WriteLine(datos);
+				}
+			}
+			catch (Exception e)
+			{
+				throw new ArchivosException(e);
+			}
+			return true;
+		}
+		public bool Leer(string archivo, out string datos)
+		{
+			datos = "";
+			try
+			{
+				using (StreamReader textArch = new StreamReader(archivo))
+				{
+					datos = textArch.ReadToEnd();
+				}
+			}
+			catch (Exception e)
+			{
+				throw new ArchivosException(e);
+			}
+			return true;
+		} 
+		#endregion
+	}
+}
---
+++ [LAB II]  TP3.Leonardo.Manassali.2A/TP-Finalizado/Archivos/Xml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using Excepciones;
+
+namespace Archivos
+{
+	public class Xml<T> : IArchivo<T>
+	{
+		#region MetodosIArchivo
+		public bool Guardar(string archivo, T datos)
+		{
+			try
+			{
+				using (StreamWriter stremWriter = new StreamWriter(archivo))
+				{
+					XmlSerializer xml = new XmlSerializer(typeof(T));
+					xml.Serialize(stremWriter, datos);
+				}
+			}
+			catch (Exception e )
+			{
+				throw new ArchivosException(e);
+			}
+			return true;
+		}
+		public bool Leer(string archivo, out T datos)
+		{
+			try
+			{
+				using (StreamReader stremWriter = new StreamReader(archivo))
+				{
+					XmlSerializer xml = new XmlSerializer(typeof(T));
+					datos = (T)xml.Deserialize(stremWriter);
+				}
+			}
+			catch (Exception e)
+			{
+				throw new ArchivosException(e);
+			}
+			return true;
+		} 
+		#endregion
+	}
+}
